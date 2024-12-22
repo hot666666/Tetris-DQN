6de7e760980b6cc11c2bcbd9d54eca3205bc0a12
hot666666 <hotcs6@naver.com>
2024-12-22 14:44:23 +0900

    feat: enhance DNN architecture with additional linear layer and ReLU activation

--- train_model.py
+++ train_model.py
@@ -61,6 +61,8 @@ class DNN(nn.Module):
         self.model = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
+            nn.Linear(128, hidden_dim),
+            nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, output_dim)
